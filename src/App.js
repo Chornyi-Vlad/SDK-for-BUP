2ce1d7a3e258882000ab7add05e6b9a563973fb9
Chornyi Vlad <VladislavC@ein-des-ein.com>
2022-02-15 14:29:03 +0200

    +

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  useEffect(() => {
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: "470141627940051",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v11.0",
+      });
+    };
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
+
+  const getData = () => {
+    axios
+      .get(
+        "https://api.instagram.com/oauth/authorize?client_id=1888342354683903&redirect_uri=http://localhost:3000&scope=user_profile,user_media&response_type=code"
+      )
+      .then((response) => {
+        console.log(response);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div
+          className="fb-login-button"
+          data-width=""
+          data-size="large"
+          data-button-type="continue_with"
+          data-layout="default"
+          data-auto-logout-link="false"
+          data-use-continue-as="false"
+        ></div>
+        <a
+          target="_blanc"
+          href="https://api.instagram.com/oauth/authorize
+                ?client_id=1888342354683903
+                &redirect_uri=https://localhost:3000
+                &scope=user_profile,user_media
+                &response_type=code"
+        >
+          link
+        </a>
+        <button type="button" onClick={getData}>
+          GOOOOOOO
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
