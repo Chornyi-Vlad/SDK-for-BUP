45581e40c60a218a1838106055fce3fec0acefdc
Chornyi Vlad <VladislavC@ein-des-ein.com>
2022-02-16 11:06:32 +0200

    +

--- src/Auth/Auth.jsx
+++ src/Auth/Auth.jsx
@@ -7,19 +7,19 @@ const Auth = () => {
   const code = location.search.slice(6);
 
   useEffect(() => {
-    code ??
-      axios
-        .post({
-          url: "https://api.instagram.com/oauth/access_token",
-          data: {
-            client_id: 1888342354683903,
-            client_secret: "5ce1d616eba6db19d2cd20e55166b20d",
-            code,
-            grant_type: "authorization_code",
-            redirect_uri: "https://socialsdk.herokuapp.com/auth/",
-          },
-        })
-        .then((response) => console.log(response.data));
+    console.log(code);
+    axios
+      .post({
+        url: "https://api.instagram.com/oauth/access_token",
+        data: {
+          client_id: 1888342354683903,
+          client_secret: "5ce1d616eba6db19d2cd20e55166b20d",
+          code,
+          grant_type: "authorization_code",
+          redirect_uri: "https://socialsdk.herokuapp.com/auth/",
+        },
+      })
+      .then((response) => console.log(response.data));
   }, []);
 
   return (
